6323a1c64c3b9bb18a81ebc7a64b54b757a52dfe
Gayan Weerakutti <gayan@linuxdeveloper.space>
2018-08-14 17:06:07 +0530

    Update cibuild.sh

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -20,7 +20,7 @@ git clone https://${GH_TOKEN}@github.com/linuxdevspace/blog.git --branch gh-page
 gem install bundler
 
 #  make sure all dependencies in the Gemfile are available 
-# bundle install --deployment
+bundle install --no-deployment
 
 # update all gems to the latest 
 if [ $BUNDLE_UPDATE == "true" ]; then
