fa6c7c2d0c90b825a12de178f08e49cdf8091e92
Gayan Weerakutti <gayan@linuxdeveloper.space>
2018-08-14 15:30:11 +0530

    Update cibuild.sh

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -18,7 +18,7 @@ mkdir _site
 git clone https://${GH_TOKEN}@github.com/linuxdevspace/blog.git --branch gh-pages _site
 
 # update all gems to the latest 
-if [ $BUNDLE-UPDATE == "true" ]; then
+if [ $BUNDLE_UPDATE == "true" ]; then
   bundle update
 fi
 
